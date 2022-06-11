ef2a1944b9ab9c512e26a97aa9913b284bf18746
gayathri <gaya.yt@gmail.com>
2022-06-11 23:07:28 +0530

    Add missing class

---
+++ src/main/java/metadata/Constants.java
@@ -0,0 +1,17 @@
+package metadata;
+
+/**
+ * This class defines all the constants which are used in the project
+ *
+ * @author Gayathri Gamage
+ * Date 11 June 2022
+ */
+public class Constants {
+
+    public static final String CATEGORY_SEARCH_IP = "CATEGORY_SEARCH_IP";
+    public static final String CATEGORY_SEARCH_RESOURCE = "CATEGORY_SEARCH_RESOURCE";
+    public static final String CATEGORY_SEARCH_QUERY_PARAMS = "CATEGORY_SEARCH_QUERY_PARAMS";
+    public static final String CATEGORY_SEARCH_END_POINT = "CATEGORY_SEARCH_URL";
+
+
+}
